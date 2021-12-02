f22dd6efa08dd1728588258871ae8fc2a29dd1f3
Anand Upadhya <anand.upadhya@yahoo.com>
2021-12-02 18:04:09 +0800

    Remove reverse geocode and add render_error method

--- app/controllers/api/v1/toilets_controller.rb
+++ app/controllers/api/v1/toilets_controller.rb
@@ -26,4 +26,9 @@ class Api::V1::ToiletsController < Api::V1::BaseController
   def toilet_params
     params.require(:toilet).permit(:address, :latitude, :longitude, :accessibility, :changing_station, :directions, :male, :female, :approved, :description, :user_id)
   end
+
+  def render_error
+    render json: { errors: @toilet.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end
